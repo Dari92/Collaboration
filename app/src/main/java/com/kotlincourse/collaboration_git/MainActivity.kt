55355decd181b607d6109d3b18d24bded8786bde
Dariana Vielma <dariana.vielma92@outlook.com>
2020-08-30 15:44:20 -0400

    First commit

---
+++ app/src/main/java/com/kotlincourse/collaboration_git/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlincourse.collaboration_git
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
